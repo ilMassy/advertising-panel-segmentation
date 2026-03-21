4bea374c825051196e8311ed3055ac0844b3ebf3
ilMassy <massimilianogiangreco7@gmail.com>
2026-03-21 21:54:22 +0000

    Add MMSegmentation config files for B0 baseline and B1 standard

--- configs/segformer_b0_baseline.py
+++ configs/segformer_b0_baseline.py
@@ -63,6 +63,16 @@ test_evaluator = val_evaluator
 
 # Model: 2 classes with pretrained backbone
 model = dict(
+    data_preprocessor=dict(
+        type='SegDataPreProcessor',
+        mean=[123.675, 116.28, 103.53],
+        std=[58.395, 57.12, 57.375],
+        bgr_to_rgb=True,
+        pad_val=0,
+        seg_pad_val=255,
+        size=crop_size,        # explicitly set size
+        size_divisor=None,     # explicitly unset size_divisor
+    ),
     backbone=dict(
         init_cfg=dict(
             type='Pretrained',
